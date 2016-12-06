c687f1af0c52d6924a72f4abcffa7c9d002a2d1b
mebartsch <spamblocka200@gmail.com>
2016-12-06 11:19:51 -0800

    Update counting_rabbits.js

--- scripts/counting_rabbits.js
+++ scripts/counting_rabbits.js
@@ -2,5 +2,6 @@ function count_rabbits() {
     for(var i=1; i<=3; i++) {
         // operator + concatenates strings
         alert("Rabbit "+i+" out of the hat!")
+        document.write("Rabbit "+i+" out of the hat!")
     }
 }
